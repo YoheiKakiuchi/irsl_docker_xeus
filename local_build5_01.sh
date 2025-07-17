3e00074e5d1e2b7ea313cc5dbb7f9fdeed4f9146
Yohei Kakiuchi <kakiuchi.yohei.sw@tut.jp>
2025-07-18 00:39:42 +0900

    update local_build script

--- local_build5_01.sh
+++ local_build5_01.sh
@@ -12,7 +12,7 @@ OUTPUT_DIR=/tmp/xeus5
 (mkdir xtl && wget https://github.com/xtensor-stack/xtl/archive/refs/tags/0.7.7.tar.gz --quiet -O - | tar zxf - --strip-components 1 -C xtl)
 (cd xtl; cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${OUTPUT_DIR} . ; make install -j$(nproc) )
 ## xeus / 5.1.1
-(mkdir xeus && wget https://github.com/jupyter-xeus/xeus/archive/refs/tags/5.1.1.tar.gz --quiet -O - | tar zxf - --strip-components 1 -C xeus)
+(mkdir xeus && wget https://github.com/jupyter-xeus/xeus/archive/refs/tags/5.2.3.tar.gz --quiet -O - | tar zxf - --strip-components 1 -C xeus)
 (cd xeus; cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${OUTPUT_DIR} . ; make install -j$(nproc) )
 
 ### xeus-zmp
@@ -25,3 +25,17 @@ OUTPUT_DIR=/tmp/xeus5
 ## xeus-zmq
 (mkdir -p xeus-zmq/build && wget https://github.com/jupyter-xeus/xeus-zmq/archive/refs/tags/3.1.0.tar.gz --quiet -O - | tar zxf - --strip-components 1 -C xeus-zmq)
 (cd xeus-zmq/build; cmake -DCMAKE_PREFIX_PATH=${OUTPUT_DIR} -DCMAKE_INSTALL_PREFIX=${OUTPUT_DIR} -DCMAKE_BUILD_TYPE=Release ..; make install -j$(nproc) )
+
+### xeus-python
+## cmake
+(mkdir -p /tmp/cmake; wget https://github.com/Kitware/CMake/releases/download/v3.30.3/cmake-3.30.3-linux-x86_64.tar.gz --quiet -O - | tar zxf - --strip-components 1 -C /tmp/cmake )
+## pybind11
+#(mkdir -p pybind11/build && wget https://github.com/pybind/pybind11/archive/refs/tags/v2.12.0.tar.gz --quiet -O - | tar zxf - --strip-components 1 -C pybind11 )## cnoid_compatible?
+(mkdir -p pybind11/build && wget https://github.com/pybind/pybind11/archive/refs/tags/v2.13.6.tar.gz --quiet -O - | tar zxf - --strip-components 1 -C pybind11 )
+(cd pybind11/build; /tmp/cmake/bin/cmake .. -DCMAKE_PREFIX_PATH=${OUTPUT_DIR} -DCMAKE_INSTALL_PREFIX=${OUTPUT_DIR} -DPYBIND11_TEST=OFF; make install)
+## pybind11_json
+(mkdir -p pybind11_json/build && wget https://github.com/pybind/pybind11_json/archive/refs/tags/0.2.15.tar.gz --quiet -O - | tar zxf - --strip-components 1 -C pybind11_json )
+(cd pybind11_json/build; /tmp/cmake/bin/cmake .. -DCMAKE_PREFIX_PATH=${OUTPUT_DIR} -DCMAKE_INSTALL_PREFIX=${OUTPUT_DIR}; make install)
+## xeus-python
+(mkdir -p xeus-python/build && wget https://github.com/jupyter-xeus/xeus-python/archive/refs/tags/0.17.4.tar.gz --quiet -O - | tar zxf - --strip-components 1 -C xeus-python)
+(cd xeus-python/build; /tmp/cmake/bin/cmake .. -DCMAKE_PREFIX_PATH=${OUTPUT_DIR} -DCMAKE_INSTALL_PREFIX=${OUTPUT_DIR} -DCMAKE_INSTALL_LIBDIR=lib -DPYTHON_EXECUTABLE=`which python3`; make install -j$(nproc) )
